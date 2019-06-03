54e6383f5787ee2cab8b71300f46f880aa6d128d
Mark Lokhorst <mlokhors@student.codam.nl>
2019-06-03 18:22:09 +0200

    maandag 3 juni

--- fillit_dir/zeewier_auxiliary.c
+++ fillit_dir/zeewier_auxiliary.c
@@ -1,21 +1,23 @@
 
 #include "fillit.h"
 
-void  cubes_offsetter(uint64_t tetrimino, int **cubes)
+void  cubes_offsetter(uint64_t tetrimino, int cubes[4][2])
 {
 	int i;
 	int count;
 	int count2;
 	int	nl;
 
-	nl = -1;
+	nl = 0;
 	i = 0;
 	count = 0;
+	count2 = 0;
 	while (i < 64)
 	{
-		if (tetrimino & 1ull >> i)
+		if (tetrimino & (1ull << (63 - i)))
 		{
-			nl = i / 16;
+			nl = 0;
+			nl = ((i + 1)/ 16);
 			cubes[count][count2] = nl;
 			count2++;
 			cubes[count][count2] = i - (nl * 16);
@@ -31,7 +33,7 @@ void  cubes_offsetter(uint64_t tetrimino, int **cubes)
 		count2 = 0;
 		while (count2 < 2)
 		{
-			cubes[count][count2] = cubes[3][count2] - cubes[count][count2];
+			cubes[count][count2] = cubes[count][count2] - cubes[3][count2];
 			count2++;
 		}
 		count++;
